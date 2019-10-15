2d687c52cd6c07cb47d276e2833f4137d9647124
bio-boris <bio-boris@users.noreply.github.com>
2019-10-15 22:54:39 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,51 @@
+//ci.kbase.us', 'appdev.kbase.us', 'next.kbase.us',
+var environments = [ 'kbase.us'];
+var services = ['userandjobstate', 'njs_wrapper', 'ws', 'auth'];
+var urls = {};
+
+environments.forEach(function (env) {
+  urls[env] = [];
+  services.forEach(function (service) {
+    var url = "https://" + env + "/" + "services/" + service;
+    console.log(url)
+    urls[env].push(url);
+    var env_name = env.split(".")[0]
+    $("#" + env_name).append("<br>" + url);
+
+    fetch(url, {
+      mode: 'no-cors',
+      //same-site : null,
+      credentials: "include"
+    })
+      // .then((resp) => resp.json()) // Transform the data into json
+      .then(function (data) {
+        $("#" + env_name).append(data);
+      })
+
+
+      //   $.ajax({
+  //   url: url,
+  //     crossDomain: true,
+  //   context: document.body
+  // }).done(function() {
+  //   $("#" + env_name).append(data);
+  // });
+
+
+    // ajax(url, {
+    //   mode: 'no-cors'
+    // })
+    //   //.then((resp) => resp.json()) // Transform the data into json
+    //   .then(function (data) {
+    //
+    //   })
+    // .catch(function() {
+    //     console.log("error");
+    // });
+
+
+  })
+
+})
+
 
